cdb6c9c1d532d66e1cedfcb43859c15a99de66a1
frisciliasultan <frisciliasultan@gmail.com>
2020-07-05 01:50:28 +0800

    remove axios proxy

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -4,7 +4,6 @@ const setAuthToken = token => {
     if(token) {
         //Apply authorization token to every request if logged in 
         axios.defaults.headers.common["Authorization"] = token;
-        axios.defaults.proxy = "https://modtree-api.netlify.app/.netlify/functions"
     } else {
         //Delete auth header
         delete axios.defaults.headers.common["Authorization"];
