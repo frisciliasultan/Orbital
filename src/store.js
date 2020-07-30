4ca976f73b41fbfcdc89372f7c7b446bc1d186e1
frisciliasultan <frisciliasultan@gmail.com>
2020-07-30 15:41:26 +0800

    typo

--- src/store.js
+++ src/store.js
@@ -4,24 +4,17 @@ import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
-
 export const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
   // compose(
-<<<<<<< HEAD
-    applyMiddleware(...middleware),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-=======
   //   applyMiddleware(...middleware),
   //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   // )
   composeWithDevTools(applyMiddleware(...middleware))
 );
->>>>>>> hosting
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(
